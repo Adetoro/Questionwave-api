d4d6c4b49c03b754970c06ff22b7083f89f3431b
adetoro <torokoga@gmail.com>
2021-06-04 18:17:59 +0100

    updated server

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ const db = knex({
   }
 });
 
+//process.env.NODE_ENV => production or undefined
+if (process.env.NODE_env === "production") {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build'));
+  });
+}
+
 
 db.select('*').from('identify').then(data => {
   console.log(data);
@@ -33,18 +42,12 @@ app.use(express.json());
 // MIDDLEWARE TO ALLOW ACCESS TO SERVER
 app.use(cors());
 
-  //app.use(express.static(path.join(__dirname, 'client/build')));
-//process.env.NODE_ENV => production or undefined
-if (process.env.NODE_env === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build'));
-  });
   
-}
 
-
+// TEST SERVER 
+app.get('/', (req, res) => {
+  res.json("it's working");
+});
    
 
 // GET LAST LINKID FROM DB  
