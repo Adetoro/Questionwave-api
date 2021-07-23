61fb0fa0e35a0f960461f726a9dfdf7eb0a52a50
adetoro <torokoga@gmail.com>
2021-07-23 19:51:26 +0100

    updated admin api call

--- client/src/components/Admin/AdminUnit.js
+++ client/src/components/Admin/AdminUnit.js
@@ -11,15 +11,15 @@ const AdminUnit = (props) => {
             <div className=" md:px-10 px-6 py-5 bg-white rounded-2xl h-auto flex items-center space-x-4 select-none ">
                 
                 <div className=" w-1/12 cellContent ">                                    
-                    {AdminLinkid} link
+                    {AdminLinkid} 
                 </div>
 
                 <div className="md:w-6/12 w-5/12 cellContent">
-                    {AdminTitle} title
+                    {AdminTitle} 
                 </div>
 
                 <div className="md:w-2/12 w-2/12 cellContent">
-                    no of q
+                    3
                 </div>
 
                 <div className="md:w-2/12 w-2/12">
