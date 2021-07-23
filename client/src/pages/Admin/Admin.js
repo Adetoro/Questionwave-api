b2852d244fe8e77df430f41f873493bcf2d866e3
adetoro <torokoga@gmail.com>
2021-07-23 03:05:00 +0100

    updated admin

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { withRouter, useHistory} from "react-router-dom";
 import "./Admin.css";
 
@@ -7,14 +7,29 @@ import Footer from '../../components/Footer/Footer';
 
 
 const Admin = (props) => {
-   let Showme =[];
+    const [AdminTitle,  setAdminTitle] =useState([]);
+    const [AdminLinkid,  setAdminLinkid] =useState([]);
+    const [AdminDate,  setAdminDate] =useState([]);
+
 
     useEffect(( ) => {
         fetch('/api/admin/') 
         .then(response => response.json())
         .then(data => {
             if(data){
-                Showme = data;
+                let n = data.length;
+                    for (let i=0; i<n; i++){
+                        if (data[i].question !== null) {
+                            //console.log("so true " + i );
+                            //SET STATE ARRAY WITH DATA FROM DATABASE
+                            setAdminTitle(AdminTitle => [...AdminTitle,data[i].title])
+                            setAdminLinkid(AdminLinkid => [...AdminLinkid,data[i].linkid])
+                            setAdminDate(AdminDate => [...AdminDate,data[i].created]);
+                        }
+                        else{
+                            console.log(" ");
+                        }
+                    }
                console.log('from admin ' + data)
               //console.log('from home linkid ' + LinkId, Title)
             }
@@ -25,8 +40,9 @@ const Admin = (props) => {
         return (
             <div id="container" className="md:w-4/5 mx-auto px-10 py-28">
                 <div className="text-xs ">
-                     
-                     {Showme}
+                     {AdminTitle}
+                     {AdminLinkid}
+                     {AdminDate}
                 </div>
                 <div className="pt-3 text-base font-normal text-center">
                      
